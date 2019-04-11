ab51f23357a618cf80150cf8645c16c3a7b45773
p3ls <p3ls@intermodi.cc>
2019-04-12 00:38:14 +0200

    no doubles, include ipv6, sort by port

--- main.go
+++ main.go
@@ -5,44 +5,34 @@ import (
     "log"
     "os/exec"
     "bytes"
+    "sort"
     "strings"
     "github.com/gosuri/uitable"
+    "menteslibres.net/gosexy/to"
     // "io"
 )
 
-var version string = "HEAD"
+var version = "HEAD"
 
-
-func netstat_info() [][]string {
-    cmd := exec.Command("netstat", "-4", "--numeric", "--all")
-    var out bytes.Buffer
-    cmd.Stdout = &out
-    err := cmd.Run()
-    if err != nil {
-        log.Fatal(err)
-    }
-    lines := strings.Split(out.String(), "\n")
-    results := make([][]string, 0)
-    for _, item := range lines {
-        if (strings.HasPrefix(item, "tcp") || strings.HasPrefix(item, "udp")) {
-            fields := strings.Fields(item) 
-            proto := fields[0]
-            local_ip_port := fields[3]
-            port := strings.Split(local_ip_port, ":")[1]
-            details := fuser_info(proto, port)
-            user := details[1]
-            // pid := details[2]
-            process := details[4]
-            results = append(results, []string{proto, port, process, user})
-            // fmt.Printf("%s %s %s\n", proto, port, process)
-           
+func Contains(a []string, x string) bool {
+    for _, n := range a {
+        if x == n {
+            return true
         }
-        
     }
-    return results
+    return false
+}
+
+func TrimSuffix(s, suffix string) string {
+    if strings.HasSuffix(s, suffix) {
+        s = s[:len(s)-len(suffix)]
+    }
+    return s
 }
 
 func fuser_info(proto string, port string) []string {
+
+    proto = TrimSuffix(proto, "6")    
     cmd := exec.Command("sudo", "fuser", "-a", "-v", fmt.Sprintf("%s/%s", port, proto))
     var out bytes.Buffer
     cmd.Stdout = &out
@@ -62,20 +52,63 @@ func fuser_info(proto string, port string) []string {
     return []string{}
 }
 
+func netstat_info() [][]string {
+    cmd := exec.Command("netstat", "-4", "-6", "--numeric", "--all")
+    var out bytes.Buffer
+    cmd.Stdout = &out
+    err := cmd.Run()
+    if err != nil {
+        log.Fatal(err)
+    }
+    lines := strings.Split(out.String(), "\n")
+    results := make([][]string, 0)
+    string_results := make([]string, 0)
+    for _, item := range lines {
+
+        if (strings.HasPrefix(item, "tcp") || strings.HasPrefix(item, "udp")) {
+            fields := strings.Fields(item) 
+            proto := TrimSuffix(fields[0], "6")
+            local_ip_port := fields[3]
+            addr := strings.Split(local_ip_port, ":")
+            port := addr[len(addr)-1]
+            // println(proto, port)
+
+            details := fuser_info(proto, port)
+            user := details[1]
+            // pid := details[2]
+            process := details[4]
+
+            id := proto + port + process + user
+            if !Contains(string_results, id){
+                results = append(results, []string{proto, port, process, user})
+                string_results = append(string_results, id)
+            }
+           
+        }
+        
+    }
+    return results
+}
+
 func main() {
 
     table := uitable.New()  
     table.MaxColWidth = 50
-    table.AddRow("lsports", "version", version, "")
+
     table.AddRow("PORT", "PROTO", "PROCESS", "USER")
-    table.AddRow("====", "=====", "=======", "====")
 
-    list_of_list_of_string := netstat_info()
-    for _, v := range list_of_list_of_string {
-        // fmt.Printf("%+q\n", v)
+    items := netstat_info()
 
+    sort.SliceStable(items, func(i, j int) bool {
+        porti := to.Int64(items[i][1])
+        portj := to.Int64(items[j][1])
+        return porti < portj
+    })
+
+    for _, v := range items {
         table.AddRow(v[1], v[0], v[2], v[3])
     }
+
     fmt.Println(table)
 
 }
